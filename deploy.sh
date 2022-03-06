30ca89caf9d410d709ab3c31bac306384186888f
ToshaShigenina <Shigenina@gmail.com>
2022-03-07 02:32:08 +0500

    change branch

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:ToshaShigenina/vue-soccer-stat.git master:gh-pages
+git push -f git@github.com:ToshaShigenina/vue-soccer-stat.git main:gh-pages
 
 cd -
